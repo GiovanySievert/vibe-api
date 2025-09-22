66590c4aeccde54dadcc36231d88b757e562b9b2
Giovany Sievert <giovanysievert@gmail.com>
2025-09-22 18:36:46 -0300

    feature: add openApi suport

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import openapi from '@elysiajs/openapi'
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Hello VibeApi').listen(3000)
+const app = new Elysia()
+  .use(openapi())
+  .get('/', () => 'Hello VibeApi')
+  .listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
