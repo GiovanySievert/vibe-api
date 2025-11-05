09ed79e4df913baeb20c8c38b183c74377aed06c
Giovany Sievert <giovanysievert@gmail.com>
2025-11-05 15:30:59 -0300

    feature: add derscription to create brand

--- src/modules/brands/http/dtos/create-brand.dto.ts
+++ src/modules/brands/http/dtos/create-brand.dto.ts
@@ -23,15 +23,20 @@ export const validateCreateVenueLocationSchema = t.Object({
   city: t.String({ minLength: 1, maxLength: 255 }),
   state: t.String({ minLength: 1, maxLength: 255 }),
   country: t.String({ minLength: 1, maxLength: 255 }),
+  neighborhood: t.String({ minLength: 1, maxLength: 255 }),
   postalCode: t.String({ minLength: 1, maxLength: 255 }),
-  lat: t.Number({ minimum: -90, maximum: 90 }),
-  lng: t.Number({ minimum: -180, maximum: 180 })
+  lat: t.String({ minimum: -90, maximum: 90 }),
+  lng: t.String({ minimum: -180, maximum: 180 })
 })
 
 export const validateBrandMenusSchema = t.Array(
   t.Object({
     name: t.String({ minLength: 1, maxLength: 255 }),
-    priceCents: t.Number({ minimum: 1, maximum: 100000 })
+    priceCents: t.Number({ minimum: 1, maximum: 100000 }),
+    description: t.String({
+      minLength: 1,
+      maxLength: 255
+    })
   })
 )
 
