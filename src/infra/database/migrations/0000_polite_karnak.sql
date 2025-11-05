447b863a168215280d20cf825e2a2c85e49464aa
Giovany Sievert <giovanysievert@gmail.com>
2025-11-05 15:30:07 -0300

    chore: remove migrations

--- src/infra/database/migrations/0000_polite_karnak.sql
+++ src/infra/database/migrations/0000_polite_karnak.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "accounts" (
-	"id" text PRIMARY KEY NOT NULL,
-	"account_id" text NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"account_id" uuid NOT NULL,
 	"provider_id" text NOT NULL,
-	"user_id" text NOT NULL,
+	"user_id" uuid NOT NULL,
 	"access_token" text,
 	"refresh_token" text,
 	"id_token" text,
@@ -15,19 +15,19 @@ CREATE TABLE "accounts" (
 );
 --> statement-breakpoint
 CREATE TABLE "sessions" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"expires_at" timestamp NOT NULL,
 	"token" text NOT NULL,
 	"created_at" timestamp NOT NULL,
 	"updated_at" timestamp NOT NULL,
 	"ip_address" text,
 	"user_agent" text,
-	"user_id" text NOT NULL,
+	"user_id" uuid NOT NULL,
 	CONSTRAINT "sessions_token_unique" UNIQUE("token")
 );
 --> statement-breakpoint
 CREATE TABLE "users" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text NOT NULL,
 	"username" text NOT NULL,
 	"email" text NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE "users" (
 );
 --> statement-breakpoint
 CREATE TABLE "verifications" (
-	"id" text PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"identifier" text NOT NULL,
 	"value" text NOT NULL,
 	"expires_at" timestamp NOT NULL,
@@ -49,16 +49,17 @@ CREATE TABLE "verifications" (
 );
 --> statement-breakpoint
 CREATE TABLE "brand_menus" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "brand_menus_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"brand_id" integer NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"brand_id" uuid NOT NULL,
 	"name" varchar(255) NOT NULL,
+	"description" varchar(255) NOT NULL,
 	"price_cents" integer NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "brands" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "brands_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"tax_id" varchar(14) NOT NULL,
 	"type" varchar(255),
@@ -68,11 +69,12 @@ CREATE TABLE "brands" (
 );
 --> statement-breakpoint
 CREATE TABLE "venue_locations" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "venue_locations_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"venue_id" integer NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"venue_id" uuid NOT NULL,
 	"address_line" varchar(255) NOT NULL,
 	"address_line_2" varchar(255),
 	"number" varchar(255),
+	"neighborhood" varchar(255),
 	"city" varchar(255) NOT NULL,
 	"state" varchar(255) NOT NULL,
 	"country" varchar(255) NOT NULL,
@@ -84,8 +86,8 @@ CREATE TABLE "venue_locations" (
 );
 --> statement-breakpoint
 CREATE TABLE "venue_opening_hours" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "venue_opening_hours_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"venue_id" integer NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"venue_id" uuid NOT NULL,
 	"weekday" integer NOT NULL,
 	"opens_at" time NOT NULL,
 	"closes_at" time NOT NULL,
@@ -95,8 +97,8 @@ CREATE TABLE "venue_opening_hours" (
 );
 --> statement-breakpoint
 CREATE TABLE "venues" (
-	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "venues_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"brand_id" integer NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"brand_id" uuid NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"price_range" varchar(5),
 	"payment_methods" varchar(255),
@@ -108,9 +110,19 @@ CREATE TABLE "venues" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "user_favorites_places" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"venue_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "user_favorites_places_user_id_venue_id_unique" UNIQUE("user_id","venue_id")
+);
+--> statement-breakpoint
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "brand_menus" ADD CONSTRAINT "brand_menus_brand_id_brands_id_fk" FOREIGN KEY ("brand_id") REFERENCES "public"."brands"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "venue_locations" ADD CONSTRAINT "venue_locations_venue_id_venues_id_fk" FOREIGN KEY ("venue_id") REFERENCES "public"."venues"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "venue_opening_hours" ADD CONSTRAINT "venue_opening_hours_venue_id_venues_id_fk" FOREIGN KEY ("venue_id") REFERENCES "public"."venues"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "venues" ADD CONSTRAINT "venues_brand_id_brands_id_fk" FOREIGN KEY ("brand_id") REFERENCES "public"."brands"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "venues" ADD CONSTRAINT "venues_brand_id_brands_id_fk" FOREIGN KEY ("brand_id") REFERENCES "public"."brands"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_favorites_places" ADD CONSTRAINT "user_favorites_places_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_favorites_places" ADD CONSTRAINT "user_favorites_places_venue_id_venues_id_fk" FOREIGN KEY ("venue_id") REFERENCES "public"."venues"("id") ON DELETE cascade ON UPDATE no action;
