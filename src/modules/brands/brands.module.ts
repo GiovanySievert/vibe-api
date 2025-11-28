6f27545e1364609518c7d641916197fd1c645d2b
Giovany Sievert <giovanysievert@gmail.com>
2025-11-28 09:21:18 -0300

    feature: add brands module

---
+++ src/modules/brands/brands.module.ts
@@ -0,0 +1,47 @@
+import {
+  CreatePlace,
+  CreatePlaceLocation,
+  CreateBrand,
+  CreateBrandMenus,
+  GetBrand,
+  CreateBrandWithPlace
+} from './application/use-cases'
+import {
+  DrizzleBrandMenusRepository,
+  DrizzlePlaceLocationsRepository,
+  DrizzlePlacesRepository,
+  DrizzleBrandRepository
+} from './infrastructure/persistence'
+import { RabbitMQProducer } from '@src/shared/infra/messaging'
+import { RabbitMQEventBus } from '@src/shared/infrastructure/rabbitmq-event-bus'
+import { BrandsController } from './http/controllers/brands.controller'
+
+export class BrandsModule {
+  public readonly controller: BrandsController
+
+  constructor() {
+    const brandRepo = new DrizzleBrandRepository()
+    const placeRepo = new DrizzlePlacesRepository()
+    const placeLocationRepo = new DrizzlePlaceLocationsRepository()
+    const brandMenusRepo = new DrizzleBrandMenusRepository()
+
+    const producer = new RabbitMQProducer()
+    const eventBus = new RabbitMQEventBus(producer)
+
+    const createBrandService = new CreateBrand(brandRepo)
+    const createBrandMenusService = new CreateBrandMenus(brandMenusRepo)
+    const createPlaceService = new CreatePlace(placeRepo)
+    const createPlaceLocationService = new CreatePlaceLocation(placeLocationRepo)
+    const getBrandService = new GetBrand(brandRepo)
+
+    const createBrandWithPlace = new CreateBrandWithPlace(
+      createBrandService,
+      createBrandMenusService,
+      createPlaceService,
+      createPlaceLocationService,
+      eventBus
+    )
+
+    this.controller = new BrandsController(createBrandWithPlace, getBrandService)
+  }
+}
--- src/modules/brands/http/routes/brands.routes.ts
+++ src/modules/brands/http/routes/brands.routes.ts
@@ -1,48 +1,9 @@
 import { Elysia, t } from 'elysia'
-
+import { BrandsModule } from '../../brands.module'
 import { validateCreateAllEntities } from '../dtos'
-import {
-  CreatePlace,
-  CreatePlaceLocation,
-  CreateBrand,
-  CreateBrandMenus,
-  GetBrand,
-  CreateBrandWithPlace
-} from '@src/modules/brands/application/use-cases'
-import {
-  DrizzleBrandMenusRepository,
-  DrizzlePlaceLocationsRepository,
-  DrizzlePlacesRepository,
-  DrizzleBrandRepository
-} from '@src/modules/brands/infrastructure/persistence'
-import { RabbitMQProducer } from '@src/shared/infra/messaging'
-import { RabbitMQEventBus } from '@src/shared/infrastructure/rabbitmq-event-bus'
-import { BrandsController } from '../controllers/brands.controller'
 
 export const brandsRoutes = (app: Elysia) => {
-  const brandRepo = new DrizzleBrandRepository()
-  const placeRepo = new DrizzlePlacesRepository()
-  const placeLocationRepo = new DrizzlePlaceLocationsRepository()
-  const brandMenusRepo = new DrizzleBrandMenusRepository()
-
-  const producer = new RabbitMQProducer()
-  const eventBus = new RabbitMQEventBus(producer)
-
-  const createBrandService = new CreateBrand(brandRepo)
-  const createBrandMenusService = new CreateBrandMenus(brandMenusRepo)
-  const createPlaceService = new CreatePlace(placeRepo)
-  const createPlaceLocationService = new CreatePlaceLocation(placeLocationRepo)
-  const getBrandService = new GetBrand(brandRepo)
-
-  const createBrandWithPlace = new CreateBrandWithPlace(
-    createBrandService,
-    createBrandMenusService,
-    createPlaceService,
-    createPlaceLocationService,
-    eventBus
-  )
-
-  const controller = new BrandsController(createBrandWithPlace, getBrandService)
+  const { controller } = new BrandsModule()
 
   return app.group('/brands', (app) =>
     app
